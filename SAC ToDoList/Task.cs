eca5858570206edcd5c0f356a58339c397206b91
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2023-01-04 15:54:14 +0100

    Creazione Task e Utilities

---
+++ SAC ToDoList/Task.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAC_ToDoList
+{
+    public class Task
+    {
+        private string taskText { get; }
+        private bool taskState { get; }
+        private DateTime? taskDate { get; }
+
+
+        //Funzione che stampa tutti gli elementi conosciuti dell'attività
+        public void PrintTaskDetails()
+        {
+            Console.WriteLine("Testo dell'attività: " + taskText);
+            Console.Write("Stato dell'attività: ");
+            if(taskState)
+            {
+                Console.WriteLine("Completato");
+            } else
+            {
+                Console.WriteLine("Non completato");
+            }
+            Console.Write("Data dell'attività: ");
+            if(taskDate != null)
+            {
+                Console.WriteLine(taskDate.ToString());
+            }
+            else
+            {
+                Console.WriteLine("non specificata");
+
+                //ciaone a tutti ciao sto scrivando dal mio computer kekw
+            }
+        }
+
+    }
+
+
+
+}
---
+++ SAC ToDoList/Utilities.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class Utilities
+    {
+
+        public static string ControllaVuoti(string value)
+        {
+            bool valido = true;
+            if (value == "")
+            {
+                valido = false;
+            }
+            while (!valido)
+            {
+                if (value == "")
+                {
+                    Console.Write("Hai inserito un testo vuoto. Riprova: ");
+                    value = Console.ReadLine();
+                }
+                else
+                {
+                    valido = true;
+                }
+            }
+            return value;
+        }
+
+        public static DateOnly ControllaData(string value)
+        {
+            CultureInfo japan = CultureInfo.GetCultureInfo("ja-JP");
+            DateOnly data = new DateOnly();
+            bool valido = DateOnly.TryParse(value, japan, DateTimeStyles.None, out data);
+            while (!valido)
+            {
+                Console.Write("Data non valida. Riprova (yyyy年mm月dd日 oppure yyyy/mm/dd): ");
+                value = Console.ReadLine();
+                valido = DateOnly.TryParse(value, japan, DateTimeStyles.None, out data);
+            }
+            return data;
+        }
+
+        public static uint ControllaUint(string value)
+        {
+            bool valido = true;
+            uint numero = 0;
+            valido = uint.TryParse(value, out numero);
+            while (!valido)
+            {
+                Console.Write("Non hai inserito un numero intelligibile. Riprova: ");
+                value = Console.ReadLine();
+                valido = uint.TryParse(value, out numero);
+            }
+            return numero;
+        }
+
+        public static double ControllaDouble(string value)
+        {
+            bool valido = true;
+            double numero = 0;
+            valido = double.TryParse(value, out numero);
+            while (!valido)
+            {
+                Console.Write("Non hai inserito un numero intelligibile. Riprova: ");
+                value = Console.ReadLine();
+                valido = double.TryParse(value, out numero);
+            }
+            return numero;
+        }
+
+        public static bool Convalida()
+        {
+            bool si = true;
+            bool valid = false;
+            do
+            {
+                string letter = Console.ReadLine().ToLower();
+                if (letter == "s" || letter == "n" || letter == "")
+                {
+                    valid = true;
+                    if (letter == "s")
+                    {
+                        si = true;
+                    }
+                    else
+                    {
+                        si = false;
+                    }
+                }
+                else
+                {
+                    Console.Write("Non è una scelta valida. Inserisci \"s\" o \"n\": ");
+                }
+            } while (!valid);
+            return si;
+        }
+
+    }
+}
