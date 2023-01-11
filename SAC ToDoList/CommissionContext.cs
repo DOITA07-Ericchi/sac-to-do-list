1181a3d58c293c9a55783414b951ca9238fdaf9f
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2023-01-11 15:16:03 +0100

    Inizializzazione del database kekw

---
+++ SAC ToDoList/CommissionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAC_ToDoList
+{
+    public class CommissionContext : DbContext
+    {
+        public DbSet<Commission> Commissions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=ericchi.moe;Database=Sessione2_SPA;User Id=SA;Password=DatabaseCaconeDegenerato;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ SAC ToDoList/Migrations/20230111141231_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SACToDoList.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commissions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commissions");
+        }
+    }
+}
