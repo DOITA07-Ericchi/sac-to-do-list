e53986bb4e77d8516343647c4c5bd44aa6fdaaee
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2023-01-11 17:28:24 +0100

    Stesura punto 7.

--- SAC ToDoList/Program.cs
+++ SAC ToDoList/Program.cs
@@ -6,8 +6,9 @@ CommissionManager menuManager = new CommissionManager();
 //RICORDATEVI DI PULLARE PRIMA DI FARE QUALSIASI COSA O VI VENGO A PICCHIARE PERSONALMENTE
 
 Console.WriteLine("Inizio programma");
-menuManager.ShowMenu();
+
 while (!menuManager.GetCloseProgramBool())
 {
+    menuManager.ShowMenu();
     menuManager.UserInput();
 }
